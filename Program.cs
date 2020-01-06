16275f04a4208bb0402e898c51ab0fd2cf0f3f5a
lexneville <jodyrichardson45@hotmail.com>
2020-01-06 12:29:39 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace abstraction
+{
+    public abstract class MovingVehicle {
+        public abstract void showMeTheVehicles();
+    }
+    public class Car : MovingVehicle {
+        public override void showMeTheVehicles() {
+            Console.WriteLine("Car");
+        }
+    }
+
+    public class Bike : MovingVehicle {
+        public override void showMeTheVehicles() {
+            System.Console.WriteLine("Bike");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MovingVehicle vehicle;
+            MovingVehicle notVehicle;
+            vehicle = new Car();
+            notVehicle = new Bike();
+            vehicle.showMeTheVehicles();
+            notVehicle.showMeTheVehicles();
+        }
+    }
+}
